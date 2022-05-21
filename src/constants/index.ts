528d3c4be34ca62226fbd0dac2dea071a3fef51c
Jin Hyeok Son <thswlsgur98@gmail.com>
2022-05-21 23:24:03 +0900

    fix: Kakao redirect url from webpack

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,7 +16,7 @@ export const KAKAO_CALLBACK_URL = "/oauth/kakao/callback";
 
 export const ERROR_MESSAGE = "올바른 작성이 필요합니다.";
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=4bd2c0bb8e2837b6462633a0ab8a7728&redirect_uri=http://${window.location.host}${KAKAO_CALLBACK_URL}callback&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=4bd2c0bb8e2837b6462633a0ab8a7728&redirect_uri=${API_URL}${KAKAO_CALLBACK_URL}callback&response_type=code`;
 
 export const DAY = ["월", "화", "수", "목", "금", "토", "일"];
 
