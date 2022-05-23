b834b73d51448904717857fd8e7363e1625d2fe2
coreight98 <maxcha98@naver.com>
2022-05-23 22:17:16 +0900

    feat : add adding travel card

--- src/components/pages/dashboard/TravelListPage.tsx
+++ src/components/pages/dashboard/TravelListPage.tsx
@@ -1,10 +1,36 @@
+import { css } from "@emotion/react";
 import { api } from "@src/app/api";
+import { theme } from "@src/styles/theme";
+import { Link } from "react-router-dom";
 
 function TravelListPage() {
   const { data: travelsData } = api.useGetTravelsQuery();
 
   return (
-    <div>
+    <div
+      css={css`
+        padding: 2rem;
+      `}
+    >
+      <Link to="/newSchedule">
+        <button
+          css={css`
+            border: none;
+            background: none;
+            border-radius: 10px;
+            min-width: 20vw;
+            min-height: 40vh;
+            box-shadow: 0px 0px 3px ${theme.colors.shadow};
+            font-size: 3rem;
+            cursor: pointer;
+            :hover {
+              opacity: 50%;
+            }
+          `}
+        >
+          +
+        </button>
+      </Link>
       {travelsData &&
         travelsData.map((travelData) => (
           <div>
