4babadc07f715a9c646f8ee05c54c297ce93a93b
sbhong <tjdqls1668@ajou.ac.kr>
2022-06-05 01:10:04 +0900

    feat: add user type in admin page

--- src/components/pages/admin/pages/User.tsx
+++ src/components/pages/admin/pages/User.tsx
@@ -2,24 +2,32 @@ import styled from "@emotion/styled";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const Img = styled.div<{img:string}>`
+const Img = styled.div<{ img: string }>`
   width: 30px;
   height: 30px;
   border-radius: 30px;
   margin-right: 10px;
-  background-image: url(${({img})=>img});
+  background-image: url(${({ img }) => img});
   background-position: center;
   background-size: cover;
 `;
 
+type UserType = {
+  userId: number;
+  profilePath: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  userType: string;
+};
+
 const User = () => {
-  const [allUsers, setAllUsers] = useState([]);
+  const [allUsers, setAllUsers] = useState<UserType[]>([]);
 
   useEffect(() => {
-    axios.get("https://api.dev.travely.guide/v1/admin/users")
-    .then(u => {
-      setAllUsers(u.data.content);
-    });
+    axios
+      .get("https://api.dev.travely.guide/v1/admin/users?page=0")
+      .then((u) => setAllUsers(u.data.content));
   }, []);
 
   return (
@@ -35,8 +43,8 @@ const User = () => {
         </tr>
       </thead>
       <tbody>
-        {allUsers.map(u =>
-          <tr>
+        {allUsers.map((u) => (
+          <tr key={u.userId}>
             <th scope="col">{u.userId}</th>
             <th scope="col">
               <Img img={u.profilePath} />
@@ -46,7 +54,7 @@ const User = () => {
             <th scope="col">{u.phoneNumber}</th>
             <th scope="col">{u.userType}</th>
           </tr>
-        )}
+        ))}
       </tbody>
     </table>
   );
