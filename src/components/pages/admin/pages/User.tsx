2d37a5bac673585a70ec89be4287ada783fb893e
sbhong <tjdqls1668@ajou.ac.kr>
2022-06-05 05:34:30 +0900

    fix: css schedule order css

--- src/components/pages/admin/pages/User.tsx
+++ src/components/pages/admin/pages/User.tsx
@@ -19,7 +19,7 @@ const User = () => {
     {
       title: "ID",
       gen: (rowData: any) => (
-        <TableCell component="th" scope="row">
+        <TableCell component="th" scope="row" align="center">
           {rowData.userId}
         </TableCell>
       ),
